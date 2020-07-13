eacbd407a51a316b804d3ff3086d717acf813cf8
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-07-13 22:02:18 +0300

    Some changes in server and working with frontend components

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div className="join-outer-container">
+      <div className="join-inner-container">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="join-input"
+            type="text"
+            onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="join-input mt-20"
+            type="text"
+            onChange={(event) => setName(event.target.value)} />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}>
+          <button className="button mt-20" type="submit">Sign in</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
