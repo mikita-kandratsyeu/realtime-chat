42898a63da7963cc7668b33496aec49ddda03841
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-07-15 01:23:11 +0300

    Fix bugs #0

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
@@ -11,6 +12,9 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+app.use(cors());
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
@@ -47,7 +51,5 @@ io.on('connect', (socket) => {
     }
   });
 });
-
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
